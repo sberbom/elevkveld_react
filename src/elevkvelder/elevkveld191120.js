43f183b20f9b7e4261e26720d39c5d272914ce54
Sigmund Berbom <sberbom@gmail.com>
2020-11-15 21:17:16 +0100

    updated feedback

--- src/elevkvelder/elevkveld191120.js
+++ src/elevkvelder/elevkveld191120.js
@@ -21,7 +21,7 @@ const Elevkveld191120 = () => {
                     <h3>Når?</h3>
                     <p className="lead">{"25. November kl 18:00 - 19:30."}</p>
                     <h3>Hvor?</h3>
-                    <p className="lead">{"Digitalt. Meld deg på nedefor, så vil du få tillsendt en link til elevkvelden. Linken kommer samme dag som elevkvelden."}</p>
+                    <p className="lead">{"Digitalt. Meld deg på nedenfor, så vil du få tilsendt en link til elevkvelden. Linken kommer samme dag som elevkvelden."}</p>
                     <h3>Påmelding</h3>
                     <a className="lead" href={"https://s.ntnu.no/elevkveld"} target="_blank" rel="noopener noreferrer">{"https://s.ntnu.no/elevkveld"}</a>
                 </div> 
@@ -36,9 +36,12 @@ const Elevkveld191120 = () => {
                 </div>
                 <div className="info-text info-text-olav">
                     <h2>Konkurranse</h2>
-                    <p className="lead">Elevkvelden avsluttes med en kahoot med fete premier! Det blir mulighet til å vinner støydempende headsett, magisk julekule og kanskje noe annet gøy!</p>
-                    <p className="lead"><a href="https://www.bose.com/en_us/products/headphones/over_ear_headphones/quietcomfort-35-wireless-ii.html#v=qc35_ii_black" target="_blank" rel="noopener noreferrer">Støydempende headsett</a></p>
-                    <p className="lead"><a href="https://www.vitenwahl.no/alle/nyheter/julekula.html" target="_blank" rel="noopener noreferrer">Magisk julekule</a></p>
+                    <p className="lead">
+                        Elevkvelden avsluttes med en Kahoot med fete premier! Det blir mulighet til å vinne støydempende headsett
+                        <a href="https://www.bose.com/en_us/products/headphones/over_ear_headphones/quietcomfort-35-wireless-ii.html#v=qc35_ii_black" target="_blank" rel="noopener noreferrer"> (Bose QuietComfort 35 II)</a>,
+                        <a href="https://www.vitenwahl.no/alle/nyheter/julekula.html" target="_blank" rel="noopener noreferrer"> magisk julekule </a>
+                        og kanskje noe annet gøy!
+                    </p>
                 </div>
             </div>
         </div>
